db7ee7908c8bd90765d49984f0822f8c7866e505
Andrew McIndoe <amcindoe@powerhrg.com>
2021-05-25 14:01:07 -0400

    superbowl

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function superbowlWin(record){
+    function hasWin(element){
+        return (element.result === `W`)
+    }
+        let superbowlWin = record.find(hasWin)
+            console.log(superbowlWin);
 
+        if(typeof superbowlWin === "object"){
+            return superbowlWin.year
+        }else {
+            return undefined
+        }
+    }
